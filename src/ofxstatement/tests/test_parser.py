e6b23859ed6e61462a4d39e32e2f9cd91c6a9d32
Zoltan Sebestyen <zoltan.sebestyen@gmail.com>
2017-12-27 19:47:04 +0100

    Mismatching id test added

--- src/ofxstatement/tests/test_parser.py
+++ src/ofxstatement/tests/test_parser.py
@@ -57,3 +57,38 @@ class ParserTest(unittest.TestCase):
             str(multi_stmt_error.exception),
             "You have more than one accounts, please configure them with "
             "ofxstatement edit-config: 12345678123456780000000, 12345678123456780000001")
+
+    def test_mismatching_account_id(self):
+        """Mismatching account id raise an exception, in CLI,
+        it looks like the following:
+        Config is
+        [unicredit-16]
+        plugin = unicredit
+        account = 12345678123456780000002
+
+        $ ofxstatement convert -t unicredit-16  ./src/ofxstatement/tests/STATEMENT_12345678_20171130.xml no.ofx
+        Traceback (most recent call last):
+          File "bin/ofxstatement", line 11, in <module>
+            load_entry_point('ofxstatement', 'console_scripts', 'ofxstatement')()
+          File "lib/python3.6/site-packages/ofxstatement-0.6.1-py3.6.egg/ofxstatement/tool.py", line 150, in run
+          File "lib/python3.6/site-packages/ofxstatement-0.6.1-py3.6.egg/ofxstatement/tool.py", line 128, in convert
+          File "lib/python3.6/site-packages/ofxstatement-0.6.1-py3.6.egg/ofxstatement/parser.py", line 23, in parse
+          File "ofxstatement/plugins/unicredit.py", line 64, in split_records
+            stmt = self._pick_matching_statement(stmts)
+          File "ofxstatement/plugins/unicredit.py", line 42, in _pick_matching_statement
+            stmt_by_acct = {_find(stmt, 'Acct/Id/Othr/Id').text: stmt for stmt in stmts}
+        Exception: The account you specified ('12345678123456780000002') is not among the ones in the file, please configure them with ofxstatement edit-config: 12345678123456780000000, 12345678123456780000001
+
+        """
+        with self.assertRaises(Exception) as mismatching_id_error:
+            filename = "./src/ofxstatement/tests/STATEMENT_12345678_20171130_multi_stmt.xml"
+            uc_parser = UnicreditParser(filename)
+            # We cheat here
+            uc_parser.account_id = '12345678123456780000002'
+            uc_parser.parse()
+
+        self.assertEqual(
+            str(mismatching_id_error.exception),
+            "The account you specified ('12345678123456780000002') is not among "
+            "the ones in the file, please configure them with "
+            "ofxstatement edit-config: 12345678123456780000000, 12345678123456780000001")
